8f45b3e8ff13a74d1739a69310df5c2f937e1a51
Kervin Levi Decena <kervin.decena@voyagerinnovation.com>
2021-03-20 11:27:18 +0800

    Initial commit

---
+++ app/src/main/java/tech/codevil/tracne/TracneApplication.kt
@@ -0,0 +1,11 @@
+package tech.codevil.tracne
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by kervin.decena on 18/03/2021.
+ */
+@HiltAndroidApp
+class TracneApplication: Application() {
+}
