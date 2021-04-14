efe26ab3dae0c1f42d6dd24f9a08eba1f00f41a5
Kervin Levi Decena <kervin.decena@voyagerinnovation.com>
2021-04-14 19:34:49 +0800

    WIP multiple graph view

--- app/src/main/java/tech/codevil/tracne/ui/statistics/GraphView.kt
+++ app/src/main/java/tech/codevil/tracne/ui/statistics/GraphView.kt
@@ -17,7 +17,6 @@ class GraphView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     val formatter = Constants.DAY_FORMAT
-    val values = listOf(8, 9, 9, 6, 5, 6, 7, 8, 4, 4, 6, 7, 8, 8, 9)
     val valuesMap = mapOf(
         "April 1, 2021" to 8,
         "April 2, 2021" to 9,
@@ -30,6 +29,8 @@ class GraphView @JvmOverloads constructor(
         "April 9, 2021" to 4,
         "April 10, 2021" to 4
     )
+    val xValues = mutableListOf<String>()
+    val xPositions = mutableListOf<Float>()
     val maxValue = 9
     var points = mutableListOf<PointF>()
 
@@ -41,10 +42,14 @@ class GraphView @JvmOverloads constructor(
         color = Color.BLUE
         style = Paint.Style.FILL
         alpha = 64
-        strokeWidth = 2f
     }
+    val xGuidePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GRAY
+        style = Paint.Style.FILL
+        alpha = 64
+    }
+
     val pointRadius = 10.0f
-    val graphPath = Path()
     val graphLinePath = Path()
 
 
@@ -59,53 +64,55 @@ class GraphView @JvmOverloads constructor(
 
     private fun computePoints() {
         points = mutableListOf()
+
+        xValues.clear()
         val today = Calendar.getInstance()
         val numberOfDays = today.getActualMaximum(Calendar.DAY_OF_MONTH)
         val iterateDate = Calendar.getInstance()
+        for (i in 1..10) {
+            iterateDate.set(Calendar.DAY_OF_MONTH, i)
+            xValues.add(formatter.format(iterateDate.time))
+            xPositions
+        }
 
-        val spacingX = width / ((numberOfDays - 1).toFloat())
+        val spacingX = width / ((xValues.size - 1).toFloat())
         val spacingY = height / ((maxValue - 1).toFloat())
 
-        graphPath.reset()
         graphLinePath.reset()
-        graphPath.moveTo(0f, height.toFloat())
         graphLinePath.moveTo(0f, height.toFloat())
         var prevX = 0f
         var prevY = height.toFloat()
-        for (i in 1..numberOfDays) {
-            iterateDate.set(Calendar.DAY_OF_MONTH, i)
-            val key = formatter.format(iterateDate.time)
+
+        xPositions.clear()
+        xValues.forEachIndexed { i, key ->
+            xPositions.add(i * spacingX)
             if (valuesMap.containsKey(key)) {
-                val x = 0 + (spacingX * (i - 1))
+                val x = i * spacingX
                 val y = (maxValue - valuesMap[key]!!) * spacingY
 
                 points.add(PointF(x, y))
-                graphPath.lineTo(x, y)
                 if (graphLinePath.isEmpty) {
                     graphLinePath.lineTo(0f, y)
-                }
-                else {
-                    graphLinePath.cubicTo((prevX + x)/2f, prevY, (prevX + x)/2f, y, x, y )
-                    //not working yet https://medium.com/@pranjalg2308/understanding-bezier-curve-in-android-and-moddinggraphview-library-a9b1f0f95cd0
+                } else {
+                    graphLinePath.cubicTo((prevX + x) / 2f, prevY, (prevX + x) / 2f, y, x, y)
+                    //https://medium.com/@pranjalg2308/understanding-bezier-curve-in-android-and-moddinggraphview-library-a9b1f0f95cd0
                 }
                 prevX = x
                 prevY = y
             }
         }
         if (points.isNotEmpty()) {
-            graphPath.lineTo(points.last().x, height.toFloat())
             graphLinePath.lineTo(points.last().x, height.toFloat())
             graphLinePath.lineTo(0f, height.toFloat())
             graphLinePath.lineTo(0f, points.first().y)
         }
-        graphPath.close()
         graphLinePath.close()
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.apply {
-//            drawPath(graphPath, graphFill)
+            xPositions.map { drawLine(it, 0f, it, height.toFloat(), xGuidePaint) }
             drawPath(graphLinePath, graphFill)
             points.map { drawCircle(it.x, it.y, pointRadius, pointPaint) }
         }
---
+++ app/src/main/java/tech/codevil/tracne/ui/statistics/MultipleGraphView.kt
@@ -0,0 +1,192 @@
+package tech.codevil.tracne.ui.statistics
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import tech.codevil.tracne.common.util.Constants
+import java.util.*
+
+class MultipleGraphView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    data class Graph(
+        val xMin: Int = 0, val xMax: Int = 10,
+        val yMin: Int = 0, val yMax: Int = 10,
+        var valuesMap: MutableMap<Int, Int>,
+        val color: Int = Color.CYAN
+    )
+    {
+        val graphPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = this@Graph.color
+            style = Paint.Style.FILL
+            alpha = 64
+        }
+
+        val pointPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = this@Graph.color
+            style = Paint.Style.FILL
+        }
+    }
+
+    var sleepValuesMap = mutableMapOf(
+        1 to 8,
+        2 to 9,
+        3 to 9,
+        4 to 6,
+        5 to 6,
+        6 to 6,
+        7 to 7,
+        8 to 8,
+        9 to 6,
+        10 to 9,
+        11 to 9,
+        12 to 10,
+        13 to 6,
+        14 to 5,
+        15 to 7,
+        16 to 8,
+        17 to 8,
+        18 to 7,
+        19 to 6,
+        20 to 8,
+    )
+    val sleepGraph = Graph(
+        xMin = 1,
+        xMax = 31,
+        yMin = 0,
+        yMax = 13,
+        valuesMap = sleepValuesMap,
+        color = Color.BLUE
+    )
+
+    var spotsValuesMap = mutableMapOf(
+        1 to 0,
+        2 to 0,
+        3 to 0,
+        4 to 2,
+        5 to 3,
+        6 to 3,
+        7 to 0,
+        8 to 0,
+        9 to 1,
+        10 to 3,
+        11 to 3,
+        12 to 2,
+        13 to 2,
+        14 to 19,
+        15 to 20,
+        16 to 10,
+        17 to 1,
+        18 to 9,
+        19 to 0,
+        20 to 0,
+    )
+    val spotsGraph = Graph(
+        xMin = 1,
+        xMax = 31,
+        yMin = 0,
+        yMax = 20,
+        valuesMap = spotsValuesMap,
+        color = Color.MAGENTA
+    )
+
+
+    val graphs = mutableListOf(sleepGraph, spotsGraph)
+    val graphPaths = mutableListOf(Path(), Path())
+    val pointsList = mutableListOf(mutableListOf<PointF>(), mutableListOf<PointF>())
+    val pointRadius = 5.0f
+
+    val linePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.DKGRAY
+        style = Paint.Style.FILL
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        computePoints()
+    }
+
+    private fun computePoints() {
+        val totalWidth = width - graphStart() - graphEnd()
+        val totalHeight = height - graphTop() - graphBottom()
+        graphs.forEachIndexed { index, graph ->
+            val path = graphPaths[index]
+            val points = pointsList[index]
+
+            val spacingX = totalWidth / ((graph.xMax - graph.xMin - 1).toFloat())
+            val spacingY = totalHeight / ((graph.yMax - graph.yMin - 1).toFloat())
+
+            var prevX = graphStart()
+            var prevY = height - graphBottom()
+
+            path.reset()
+            path.moveTo(prevX, prevY)
+            points.clear()
+
+            val xValues = (graph.xMin..graph.xMax).toList()
+            xValues.forEachIndexed { i, x ->
+                if (graph.valuesMap.containsKey(x)) {
+                    val value = graph.valuesMap[x]!!
+                    val pointX = graphStart() + (i * spacingX)
+                    val pointY = (graph.yMax - value - 1) * spacingY + graphTop() //TODO: fix graphing Y
+                    points.add(PointF(pointX, pointY))
+
+                    if (path.isEmpty) {
+                        path.lineTo(prevX, pointY)
+                    } else {
+                        path.cubicTo((prevX + pointX) / 2f, prevY, (prevX + pointX) / 2f, pointY, pointX, pointY)
+                        //https://medium.com/@pranjalg2308/understanding-bezier-curve-in-android-and-moddinggraphview-library-a9b1f0f95cd0
+                    }
+                    prevX = pointX
+                    prevY = pointY
+                }
+            }
+            if (points.isNotEmpty()) {
+                path.lineTo(points.last().x, height.toFloat() - graphBottom())
+                path.lineTo(graphStart(), height.toFloat() - graphBottom())
+                path.lineTo(graphStart(), points.first().y)
+            }
+            path.close()
+        }
+
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.apply {
+            drawLine(
+                0f,
+                height.toFloat() - graphBottom(),
+                width.toFloat(),
+                height.toFloat() - graphBottom(),
+                linePaint
+            )
+            graphs.forEachIndexed { i, graph ->
+                drawPath(graphPaths[i], graph.graphPaint)
+                pointsList[i].map { drawCircle(it.x, it.y, pointRadius, graph.pointPaint) }
+            }
+        }
+    }
+
+    private fun graphStart(): Float {
+        return 50f;
+    }
+
+    private fun graphEnd(): Float {
+        return 0f;
+    }
+
+    private fun graphTop(): Float {
+        return 0f;
+    }
+
+    private fun graphBottom(): Float {
+        return 50f;
+    }
+
+}
